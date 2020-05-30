9c9fed6ab6ecd90f55310ce80b314b6dad62beb4
solfahsa <soporte@solfahsa.com>
2020-05-29 20:27:42 -0600

    matriz

---
+++ Matriz 2x2.cpp
@@ -0,0 +1,35 @@
+
+
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+	int numeros[100][100];
+	int filas,columnas;
+	
+	cout<<"Digite el numero de filas: "; cin>>filas;
+	cout<<"Digite el numero de columnas: "; cin>>columnas;
+	
+	//Rellenando la matriz	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			cout<<"Digite un numero ["<<i<<"]["<<j<<"]: ";
+			cin>>numeros[i][j];
+		}
+	} 
+	
+	cout<<"\nImprimiento matriz\n\n";
+	
+	for(int i=0;i<filas;i++){
+		for(int j=0;j<columnas;j++){
+			cout<<numeros[i][j];
+		}
+		cout<<"\n";
+	}
+	
+	
+	
+	return 0;
+}
